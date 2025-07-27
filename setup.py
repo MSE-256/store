fd2b3717ed382e5e811fe8642b19f68ea7077d9b
MSE-256 <masoledith77@gmail.com>
2025-07-27 16:22:26 -0400

    agregando seguimiento

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tienda",      # Cambia por el nombre de tu paquete
+    version="0.1.0",                # Versión inicial
+    author="NEC",             # Opcional: tu nombre o el de tu equipo
+    author_email="arenitamar7632@gmail.com",  # Opcional: tu email
+    description="venta de productos o servicios",  # Descripción breve
+    long_description="Larga descripción de lo que hace el paquete.",
+    long_description_content_type="text/markdown",  # Tipo de contenido de la descripción larga
+    url="https://github.com/MSE-256/store",  # URL de tu repositorio (si corresponde)
+    packages=find_packages(),       # Esto buscará todos los paquetes dentro del directorio
+    install_requires=[              # Lista de dependencias necesarias para el paquete
+        "django",
+        "pip",
+    ],
+    classifiers=[                   # Clasificadores (opcional)
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',         # Versión mínima de Python
+)
